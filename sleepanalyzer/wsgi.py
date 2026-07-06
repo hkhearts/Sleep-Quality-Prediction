9f32c4a88cd64a5c9d1152358a9b968d104cb555
Hari Krishnan C <iamhkhearts@gmail.com>
2026-07-06 10:51:05 +0530

    Second Comm

--- sleepanalyzer/wsgi.py
+++ sleepanalyzer/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Add project root to sys.path
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sleepanalyzer.settings')
 
 application = get_wsgi_application()
+
+# Vercel serverless function expects an 'app' variable
+app = application
